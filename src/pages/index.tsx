2361a220f1c96e14fc333cc2c675f690898b8704
Marcos Corsi <marcosvcorsi@gmail.com>
2020-08-29 20:08:56 -0300

    feat: finished example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
 
+import { Container } from '../styles/pages/Home';
+
 const Home: React.FC = () => {
   return (
-    <div>
+    <Container>
       <Head>
         <title>Home Page</title>
       </Head>
 
-      <main>
-        <h1>Hello World</h1>
-      </main>
-    </div>
+      <h1>ReactJS Structure</h1>
+      <p>A ReactJS + Next.js structure made by Rocketseat</p>
+    </Container>
   );
 };
 
